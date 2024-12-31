89323e389a1b7302ed64b82ad3ef96933e457ced
Abhijit Kumar <abhijitkumar10.5@gmail.com>
2025-01-01 00:19:21 +0530

    feat: add animation on button click

--- script.js
+++ script.js
@@ -10,8 +10,11 @@ function calculate() {
   const operator = operatorSelectInput.value
   let result
 
+  animate(false)
+
   if (isNaN(firstNumber) || isNaN(secondNumber)) {
     resultParagraph.textContent = 'Please enter a valid number'
+    animate(true)
     return
   }
 
@@ -28,6 +31,7 @@ function calculate() {
     case '/':
       if (secondNumber === 0) {
         resultParagraph.textContent = 'Division by zero is not allowed'
+        animate(true)
         return
       }
       result = firstNumber / secondNumber
@@ -40,9 +44,25 @@ function calculate() {
       break
     default:
       result = 'Invalid operator'
+      animate(true)
   }
 
   resultParagraph.textContent = 'Result: ' + result
 }
 
 calculateButton.addEventListener('click', calculate)
+
+const animate = function (isError) {
+  resultParagraph.style.transform = 'translateY(-20px)'
+  resultParagraph.style.opacity = '0'
+  isError
+    ? (resultParagraph.style.backgroundColor = '#ffb7b7')
+    : (resultParagraph.style.backgroundColor = '#aeffb2')
+  setTimeout(() => {
+    resultParagraph.style.transform = 'translateY(0)'
+    resultParagraph.style.opacity = '1'
+    isError
+      ? (resultParagraph.style.backgroundColor = '#ffb7b7')
+      : (resultParagraph.style.backgroundColor = '#aeffb2')
+  }, 100)
+}
